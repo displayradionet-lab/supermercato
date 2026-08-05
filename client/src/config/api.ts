829fc6be18bdcb8be08284c80ee9cb213d44cc4e
displayradio.net-lab <displayradio.net@gmail.com>
2026-08-05 03:18:53 -0400

    fix: set backend url fallback in api config

--- client/src/config/api.ts
+++ client/src/config/api.ts
@@ -1,31 +1,35 @@
-import axios from "axios";
+import axios from 'axios';
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_BASE_URL
-})
+  baseURL:
+    import.meta.env.VITE_BASE_URL ||
+    'https://https://supermercato-server.vercel.app/.onrender.com/api',
+});
 // Inject JWT token localStorage into every request
 api.interceptors.request.use((config) => {
-    const token = localStorage.getItem("auth_token")
-    if (token) {
-        config.headers.Authorization = `Bearer ${token}`
-    }
-    return config;
-})
+  const token = localStorage.getItem('auth_token');
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
 // handle auth errors globally
 api.interceptors.response.use(
-    (response)=> response,
-    (error) => {
-        if (error.response?.status === 401){
-            localStorage.removeItem("auth_token");
-            localStorage.removeItem("auth_user");
-            // Only redirect if not already on auth pages
-            if (!window.location.pathname.includes("/login") && 
-            !window.location.pathname.includes("/register")) {
-                window.location.href = "/login"
-            }
-        }
-        return Promise.reject(error);
+  (response) => response,
+  (error) => {
+    if (error.response?.status === 401) {
+      localStorage.removeItem('auth_token');
+      localStorage.removeItem('auth_user');
+      // Only redirect if not already on auth pages
+      if (
+        !window.location.pathname.includes('/login') &&
+        !window.location.pathname.includes('/register')
+      ) {
+        window.location.href = '/login';
+      }
     }
-)
+    return Promise.reject(error);
+  },
+);
 
 export default api;
